1d529ec227e087075011c185ecb6d50fd8d9555d
Ayu Demura <ayu.demura@gmail.com>
2016-07-21 10:12:08 -0400

    gofmt

--- volumes.go
+++ volumes.go
@@ -6,41 +6,41 @@ const volumeBasePath = "/storage"
 
 // VolumeService interface defines available Volume methods
 type VolumeService interface {
-  Get(string) (*Volume, *Response, error)
-  Update(*VolumeUpdateRequest) (*Volume, *Response, error)
-  Delete(string) (*Response, error)
-  Create(*VolumeCreateRequest) (*Volume, *Response, error)
+	Get(string) (*Volume, *Response, error)
+	Update(*VolumeUpdateRequest) (*Volume, *Response, error)
+	Delete(string) (*Response, error)
+	Create(*VolumeCreateRequest) (*Volume, *Response, error)
 }
 
 // Volume represents a volume
 type Volume struct {
-  ID               string           `json:"id"`
-  Name             string           `json:"name,omitempty"`
-  Description      string           `json:"description,omitempty"`
-  Size             int              `json:"size,omitempty"`
-  State            string           `json:"state,omitempty"`
-  Locked           bool             `json:"locked,omitempty"`
-  BillingCycle     string           `json:"billing_cycle,omitempty"`
-  Created          string           `json:"created_at,omitempty"`
-  Updated          string           `json:"updated_at,omitempty"`
-  Href             string           `json:"href,omitempty"`
-  SnapshotPolicies []SnapshotPolicy `json:"snapshot_policies,omitempty"`
-  Attachments      []Attachment     `json:"attachments,omitempty"`
-  Plan             *Plan            `json:"plan,omitempty"`
-  Facility         *Facility        `json:"facility,omitempty"`
-  Project          *Project         `json:"project,omitempty"`
+	ID               string           `json:"id"`
+	Name             string           `json:"name,omitempty"`
+	Description      string           `json:"description,omitempty"`
+	Size             int              `json:"size,omitempty"`
+	State            string           `json:"state,omitempty"`
+	Locked           bool             `json:"locked,omitempty"`
+	BillingCycle     string           `json:"billing_cycle,omitempty"`
+	Created          string           `json:"created_at,omitempty"`
+	Updated          string           `json:"updated_at,omitempty"`
+	Href             string           `json:"href,omitempty"`
+	SnapshotPolicies []SnapshotPolicy `json:"snapshot_policies,omitempty"`
+	Attachments      []Attachment     `json:"attachments,omitempty"`
+	Plan             *Plan            `json:"plan,omitempty"`
+	Facility         *Facility        `json:"facility,omitempty"`
+	Project          *Project         `json:"project,omitempty"`
 }
 
 // SnapshotPolicy used to execute actions on volume
 type SnapshotPolicy struct {
-  ID    string    `json:"id"`
-  Href  string    `json:"href"`
+	ID   string `json:"id"`
+	Href string `json:"href"`
 }
 
 // Attachment used to execute actions on volume
 type Attachment struct {
-  ID    string    `json:"id"`
-  Href  string    `json:"href"`
+	ID   string `json:"id"`
+	Href string `json:"href"`
 }
 
 func (v Volume) String() string {
@@ -49,12 +49,12 @@ func (v Volume) String() string {
 
 // VolumeCreateRequest type used to create a Packet volume
 type VolumeCreateRequest struct {
-  Size          int      `json:"size"`
-  BillingCycle  string   `json:"billing_cycle"`
-  ProjectID     string   `json:"project_id"`
-  PlanID        string   `json:"plan_id"`
-  FacilityID    string   `json:"facility_id"`
-  Description   string   `json:"Description,omitempty"`
+	Size         int    `json:"size"`
+	BillingCycle string `json:"billing_cycle"`
+	ProjectID    string `json:"project_id"`
+	PlanID       string `json:"plan_id"`
+	FacilityID   string `json:"facility_id"`
+	Description  string `json:"Description,omitempty"`
 }
 
 func (v VolumeCreateRequest) String() string {
@@ -63,9 +63,9 @@ func (v VolumeCreateRequest) String() string {
 
 // VolumeUpdateRequest type used to update a Packet volume
 type VolumeUpdateRequest struct {
-	ID            string   `json:"id"`
-	Description   string   `json:"description,omitempty"`
-	Plan          string   `json:"plan,omitempty"`
+	ID          string `json:"id"`
+	Description string `json:"description,omitempty"`
+	Plan        string `json:"plan,omitempty"`
 }
 
 func (v VolumeUpdateRequest) String() string {
