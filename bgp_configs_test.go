ef7bb2c57667f21fc62adf836c0e5e5a9cacfc22
jasminSPC <jasmin@stackpointcloud.com>
2018-07-24 22:42:55 +0200

    renamed files

---
+++ bgp_configs_test.go
@@ -0,0 +1,45 @@
+package packngo
+
+import (
+	"testing"
+)
+
+func TestAccBGPConfig(t *testing.T) {
+	skipUnlessAcceptanceTestsAllowed(t)
+
+	c, projectID, _ := setupWithProject(t)
+
+	configRequest := CreateBGPConfigRequest{
+		DeploymentType: "local",
+		Asn:            65000,
+		Md5:            "c3RhY2twb2ludDIwMTgK",
+	}
+
+	_, err := c.BGPConfig.Create(projectID, configRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	config, _, err := c.BGPConfig.Get(projectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if config == nil {
+		t.Fatal("BGP config not retrieved")
+	}
+
+	if config.Md5 != configRequest.Md5 {
+		t.Fatal("BGP config is not set up properly")
+	}
+
+	if config.Asn != configRequest.Asn {
+		t.Fatal("BGP config is not set up properly")
+	}
+
+	_, err = c.BGPConfig.Delete(config.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	projectTeardown(c)
+}
