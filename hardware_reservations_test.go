11457edbf8ee380fb99c095e176b4f8c0ac6c216
jasminSPC <jasmin@stackpointcloud.com>
2018-08-06 16:06:19 +0200

    Fixed tests for hardware reservations

--- hardware_reservations_test.go
+++ hardware_reservations_test.go
@@ -6,33 +6,14 @@ import (
 
 func TestAccListHardwareReservations(t *testing.T) {
 	skipUnlessAcceptanceTestsAllowed(t)
-	c := setup(t)
-
-	projects, _, err := c.Projects.List(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(projects) == 0 {
-		t.Fatal("No projects returned.")
-	}
-
-	projectID := projects[0].ID
+	c, projectID, tearDown := setupWithProject(t)
+	defer tearDown()
 
 	hardwareReservations, _, err := c.HardwareReservations.List(projectID, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	hrID := hardwareReservations[0].ID
-
-	hardwareReservation, _, err := c.HardwareReservations.Get(hrID, nil)
-	if err != nil {
-		t.Fatal(err)
+	if len(hardwareReservations) != 0 {
+		t.Fatal("There should not be any hardware reservations.")
 	}
-
-	if hardwareReservation.ID != hrID {
-		t.Fatal("Hardware reservation IDs don't match.")
-	}
-
 }
