b8bfa4bf3293ed0020eb16cdc6870cce9bd3ea05
jasminSPC <jasmin@stackpointcloud.com>
2018-08-14 13:41:59 +0200

    Added ReceiveSms and ReceiveApp, removed unit tests for 2FA

--- packngo.go
+++ packngo.go
@@ -237,10 +237,28 @@ func (c *Client) DoRequest(method, path string, body, v interface{}) (*Response,
 	return c.Do(req, v)
 }
 
+// DoRequestWithHeader same as DoRequest
+func (c *Client) DoRequestWithHeader(method string, headers map[string]string, path string, body, v interface{}) (*Response, error) {
+	req, err := c.NewRequest(method, path, body)
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	if c.debug {
+		o, _ := httputil.DumpRequestOut(req, true)
+		log.Printf("\n=======[REQUEST]=============\n%s\n", string(o))
+	}
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req, v)
+}
+
+// NewClient initializes and returns a Client
 func NewClient() (*Client, error) {
 	apiToken := os.Getenv(packetTokenEnvVar)
 	if apiToken == "" {
-		return nil, fmt.Errorf("you must export %s.", packetTokenEnvVar)
+		return nil, fmt.Errorf("you must export %s", packetTokenEnvVar)
 	}
 	c := NewClientWithAuth("packngo lib", apiToken, nil)
 	return c, nil
--- two_factor_auth.go
+++ two_factor_auth.go
@@ -5,10 +5,12 @@ const twoFactorAuthSmsPath = "/user/otp/sms"
 
 // TwoFactorAuthService interface defines available two factor authentication functions
 type TwoFactorAuthService interface {
-	EnableApp() (*Response, error)
-	DisableApp() (*Response, error)
-	EnableSms() (*Response, error)
-	DisableSms() (*Response, error)
+	EnableApp(string) (*Response, error)
+	DisableApp(string) (*Response, error)
+	EnableSms(string) (*Response, error)
+	DisableSms(string) (*Response, error)
+	ReceiveSms() (*Response, error)
+	ReceiveApp() (string, *Response, error)
 }
 
 // TwoFactorAuthServiceOp implements TwoFactorAuthService
@@ -17,21 +19,38 @@ type TwoFactorAuthServiceOp struct {
 }
 
 // EnableApp function enables two factor auth using authenticatior app
-func (s *TwoFactorAuthServiceOp) EnableApp() (resp *Response, err error) {
-	return s.client.DoRequest("POST", twoFactorAuthAppPath, nil, nil)
+func (s *TwoFactorAuthServiceOp) EnableApp(token string) (resp *Response, err error) {
+	headers := map[string]string{"x-otp-token": token}
+	return s.client.DoRequestWithHeader("POST", headers, twoFactorAuthAppPath, nil, nil)
 }
 
 // EnableSms function enables two factor auth using sms
-func (s *TwoFactorAuthServiceOp) EnableSms() (resp *Response, err error) {
-	return s.client.DoRequest("POST", twoFactorAuthSmsPath, nil, nil)
+func (s *TwoFactorAuthServiceOp) EnableSms(token string) (resp *Response, err error) {
+	headers := map[string]string{"x-otp-token": token}
+	return s.client.DoRequestWithHeader("POST", headers, twoFactorAuthSmsPath, nil, nil)
+}
+
+// ReceiveSms orders the auth service to issue an SMS token
+func (s *TwoFactorAuthServiceOp) ReceiveSms() (resp *Response, err error) {
+	return s.client.DoRequest("POST", twoFactorAuthSmsPath+"/receive", nil, nil)
 }
 
 // DisableApp function disables two factor auth using
-func (s *TwoFactorAuthServiceOp) DisableApp() (resp *Response, err error) {
-	return s.client.DoRequest("DELETE", twoFactorAuthAppPath, nil, nil)
+func (s *TwoFactorAuthServiceOp) DisableApp(token string) (resp *Response, err error) {
+	headers := map[string]string{"x-otp-token": token}
+	return s.client.DoRequestWithHeader("DELETE", headers, twoFactorAuthAppPath, nil, nil)
 }
 
 // DisableSms function disables two factor auth using
-func (s *TwoFactorAuthServiceOp) DisableSms() (resp *Response, err error) {
-	return s.client.DoRequest("DELETE", twoFactorAuthSmsPath, nil, nil)
+func (s *TwoFactorAuthServiceOp) DisableSms(token string) (resp *Response, err error) {
+	headers := map[string]string{"x-otp-token": token}
+	return s.client.DoRequestWithHeader("DELETE", headers, twoFactorAuthSmsPath, nil, nil)
+}
+
+// ReceiveApp orders the auth service to issue an SMS token
+func (s *TwoFactorAuthServiceOp) ReceiveApp() (otpURI string, resp *Response, err error) {
+	ret := &map[string]string{}
+	resp, err = s.client.DoRequest("POST", twoFactorAuthAppPath+"/receive", nil, ret)
+
+	return (*ret)["otp_uri"], resp, err
 }
