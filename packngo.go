a87ba33242d8b49263988b7c4344039ce8af07a6
Marques Johansson <mjohansson@equinix.com>
2020-10-22 21:58:23 -0400

    v0.5.0 version bump (was 0.4.2)
    Bumping to 0.5.0 because of breaking changes with ConvertDevice
    
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- packngo.go
+++ packngo.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	authTokenEnvVar = "PACKET_AUTH_TOKEN"
-	libraryVersion  = "0.4.2"
+	libraryVersion  = "0.5.0"
 	baseURL         = "https://api.equinix.com/metal/v1/"
 	userAgent       = "packngo/" + libraryVersion
 	mediaType       = "application/json"
