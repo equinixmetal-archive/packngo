7727bbb8844954d9c6dba5fbbd839d2387051c24
Tomas Karasek <tom.to.the.k@gmail.com>
2020-11-30 17:28:09 +0100

    v0.5.1 version bump
    Signed-off-by: Tomas Karasek <tom.to.the.k@gmail.com>

--- packngo.go
+++ packngo.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	authTokenEnvVar = "PACKET_AUTH_TOKEN"
-	libraryVersion  = "0.5.0"
+	libraryVersion  = "0.5.1"
 	baseURL         = "https://api.equinix.com/metal/v1/"
 	userAgent       = "packngo/" + libraryVersion
 	mediaType       = "application/json"
