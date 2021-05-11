aba7afab1bfbd8d6a4a8b8d46fe2dab23b4bb22c
Marques Johansson <mjohansson@equinix.com>
2021-05-11 15:36:44 -0400

    replace Version const with a var determined at runtime
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- version.go
+++ version.go
@@ -1,4 +1,26 @@
 package packngo
 
+import "runtime/debug"
+
 // Version of the packngo package
-const Version = "0.14.1"
+var Version = "(devel)"
+
+const packagePath = "github.com/packethost/packngo"
+
+// init finds packngo in the dependency so the package Version can be properly
+// reflected in API UserAgent headers and client introspection
+func init() {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	for _, d := range bi.Deps {
+		if d.Path == packagePath {
+			Version = d.Version
+			if d.Replace != nil {
+				Version = d.Replace.Version
+			}
+			break
+		}
+	}
+}
